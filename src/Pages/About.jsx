4a783f22a1fc0d0600ada2cbbe6780fee3cd0853
Saiful-global <saifulislambappi01@gmail.com>
2026-07-11 21:35:53 +0600

    delete some not related / unnecessary files

--- src/Pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  return (
-    <div className='text-9xl text-bold text-white bg-black h-screen w-full'>This is About Page</div>
-  )
-}
-
-export default About
--- src/Pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Contact = () => {
-  return (
-    <div className='text-9xl text-bold text-white bg-black h-screen w-full'>This is Contact Page</div>
-  )
-}
-
-export default Contact
